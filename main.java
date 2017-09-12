e1e3b18fc38fd2d9bb4f180992c582388cbd74ec
SquidTheJim <30366800+SquidTheJim@users.noreply.github.com>
2017-09-12 03:11:33 -0400

    Add files via upload

---
+++ main.java
@@ -0,0 +1,45 @@
+package com.yellowplatypus.FishingHunger;
+
+import java.io.File;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerFishEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class main extends JavaPlugin implements Listener {	
+    
+    @Override
+    public void onEnable() {
+	try {
+            if (!getDataFolder().exists()) {
+                getDataFolder().mkdirs();
+            }
+            File file = new File(getDataFolder(), "config.yml");
+            if (!file.exists()) {
+                getLogger().info("Creating Config.yml");
+                saveDefaultConfig();
+            }   
+        } 
+        catch (Exception e) {}
+	Bukkit.getServer().getConsoleSender().sendMessage("[FishingHunger] Enabled.");
+	Bukkit.getServer().getPluginManager().registerEvents(this, this);
+    }
+    
+    @Override
+    public void onDisable() {
+        getServer().getConsoleSender().sendMessage("[FishingHunger] Disabled.");
+    }
+    
+    @EventHandler
+    public void onPlayerFish(PlayerFishEvent event) {
+        if (event.getState().equals(PlayerFishEvent.State.FISHING)){
+            Player player = event.getPlayer();  
+            float penalty = (float)getConfig().getDouble("penalty");
+            float exhaustion = player.getExhaustion();
+            player.setExhaustion(exhaustion + penalty);
+        }
+    }    
+}   
